35662f675b843cfa158e17d0c00b08af96edb0b4
Joao Prado <joaopradocampos@gmail.com>
2025-11-30 17:55:18 -0300

    feat: Implementação AeroportoUpdateDTO.java

---
+++ src/main/java/com/exemplo/app/dto/AeroportoUpdateDTO.java
@@ -0,0 +1,15 @@
+package com.exemplo.app.dto;
+
+import java.math.BigDecimal;
+
+public record AeroportoUpdateDTO(
+        Integer idAeroporto,
+        String nomeAeroporto,
+        String codigoIATA,
+        String cidade,
+        String codigoISO,
+        BigDecimal latitude,
+        BigDecimal longitude,
+        BigDecimal altitude
+) {
+}
